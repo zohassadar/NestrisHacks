9c43f2e89e39dcb9482cbcb971177aee56230632
zohassadar <zohassadar@gmail.com>
2024-12-27 11:41:48 +0000

    clean up simple.sh

--- simple.sh
+++ simple.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
-# AnyDAS rom with tournament friendly features
+# BUILDFLAGS are passed to ca65 and are optional
+# OUTPUT is filename without extension
+# e.g. OUTPUT=Tetris creates Tetris.nes Tetris.dbg Tetris.lbl Tetris.map
+
+# AnyDAS rom with tournament friendly features:
 BUILDFLAGS="-D ANYDAS -D TOURNAMENT -D OMIT_UD1"
 OUTPUT=TetrisAnyTrn
 
-# Vanilla
+# Vanilla:
 # BUILDFLAGS=
 # OUTPUT=Tetris
 
-# Upside Down # BUILDFLAGS="-D UPSIDEDOWN"
+# Upside Down:
+# BUILDFLAGS="-D UPSIDEDOWN"
 # OUTPUT="UpsideDownTetris"
 
 # All Build flags (some may be incompatible!):
@@ -31,22 +36,24 @@ OUTPUT=TetrisAnyTrn
 # UPSIDEDOWN
 # WALLHACK2
 
-echo building nametables
-# modified nametables will throw warnings
+set -x
+# nametables
 python src/nametables/enter_high_score_nametable.py
 python src/nametables/game_12wide_nametable.py
 python src/nametables/game_nametable.py
 python src/nametables/game_type_menu_nametable.py
 python src/nametables/high_scores_nametable.py
 python src/nametables/legal_screen_nametable.py
-python src/nametables/level_menu_nametable.py $BUILDFLAGS
 python src/nametables/taller_game_nametable.py
-python src/nametables/title_screen_nametable.py $BUILDFLAGS
 python src/nametables/type_a_ending_nametable.py
 python src/nametables/type_b_ending_nametable.py
 python src/nametables/type_b_lvl9_ending_nametable.py
 
-echo building tilesets
+# nametables configured to accept buildflags
+python src/nametables/level_menu_nametable.py $BUILDFLAGS
+python src/nametables/title_screen_nametable.py $BUILDFLAGS
+
+# tilesets (requires the python module pillow)
 python tools/nes-util/nes_chr_encode.py src/gfx/game_tileset.png src/gfx/game_tileset.chr
 python tools/nes-util/nes_chr_encode.py src/gfx/taller_game_tileset.png src/gfx/taller_game_tileset.chr
 python tools/nes-util/nes_chr_encode.py src/gfx/title_menu_tileset.png src/gfx/title_menu_tileset.chr
@@ -54,13 +61,13 @@ python tools/nes-util/nes_chr_encode.py src/gfx/title_menu_tileset_anydas_seed.p
 python tools/nes-util/nes_chr_encode.py src/gfx/typeA_ending_tileset.png src/gfx/typeA_ending_tileset.chr
 python tools/nes-util/nes_chr_encode.py src/gfx/typeB_ending_tileset.png src/gfx/typeB_ending_tileset.chr
 
-echo assembling
-ca65 $BUILDFLAGS -g src/header.asm -o header.o
-ca65 $BUILDFLAGS -l ${OUTPUT}.lst -g src/main.asm -o main.o
+# assemble
+ca65 $BUILDFLAGS -g src/header.asm -o build/header.o
+ca65 $BUILDFLAGS -l ${OUTPUT}.lst -g src/main.asm -o build/main.o
 
-echo linking
-ld65 -m ${OUTPUT}.map \
-    -Ln ${OUTPUT}.lbl \
-    --dbgfile ${OUTPUT}.dbg \
-    -o ${OUTPUT}.nes \
+# link
+ld65 -m build/${OUTPUT}.map \
+    -Ln build/${OUTPUT}.lbl \
+    --dbgfile build/${OUTPUT}.dbg \
+    -o build/${OUTPUT}.nes \
     -C src/tetris.nes.cfg build/main.o build/header.o
