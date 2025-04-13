038c9e221d7c47c2a348fc06331a735f7ba7ad52
zohassadar <zohassadar@gmail.com>
2025-04-13 13:29:31 +0000

    fix allegro

--- src/playstate/music_speed.asm
+++ src/playstate/music_speed.asm
@@ -1,21 +1,33 @@
 ; this is bugged for TRIPLEWIDE.  fixed in the branch 30big
 
 updateMusicSpeed:
+.ifdef BIGMODE30
+        ldx     #$02
+.else
         ldx     #$05
+.endif
 .ifdef TWELVE
         lda     multBy12Table,x
+.elseif .defined(BIGMODE30)
+        lda     multBy15Table,x
 .else
         lda     multBy10Table,x
 .endif
         tay
 .ifdef TWELVE
         ldx     #$0C
+.elseif .defined(BIGMODE30)
+        ldx     #$0F
 .else
         ldx     #$0A
 .endif
 @checkForBlockInRow:
         lda     (playfieldAddr),y
+.ifdef BIGMODE30
+        cmp     #$EA
+.else
         cmp     #$EF
+.endif
         bne     @foundBlockInRow
         iny
         dex
